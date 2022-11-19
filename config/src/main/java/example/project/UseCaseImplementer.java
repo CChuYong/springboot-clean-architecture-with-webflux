ad54177d0fc62c9f8cb0a581cb56765724e74bd0
송영민 <jakgon@naver.com>
2022-11-20 03:56:49 +0900

    feat: implement read logic for user

--- config/src/main/java/example/project/UseCaseImplementer.java
+++ config/src/main/java/example/project/UseCaseImplementer.java
@@ -1,6 +1,7 @@
 package example.project;
 
 import example.project.usecase.user.CreateNewUserUseCase;
+import example.project.usecase.user.GetUserByIdUseCase;
 import example.project.usecase.user.GetUserByUserNameUseCase;
 import example.project.usecase.user.UserRepository;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,10 @@ public class UseCaseImplementer {
     @Bean
     public GetUserByUserNameUseCase getUserByUserNameUseCase(UserRepository userRepository){
         return new GetUserByUserNameUseCase(userRepository);
+    }
+    @Bean
+    public GetUserByIdUseCase getUserByIdUseCase(UserRepository userRepository){
+        return new GetUserByIdUseCase(userRepository);
     }
     @Bean
     public CreateNewUserUseCase createNewUserUseCase(UserRepository userRepository, GetUserByUserNameUseCase getUserByUserNameUseCase) {
--- infrastructure/src/main/java/example/project/infrastructure/jpa/user/JPAUserRepository.java
+++ infrastructure/src/main/java/example/project/infrastructure/jpa/user/JPAUserRepository.java
@@ -1,5 +1,6 @@
 package example.project.infrastructure.jpa.user;
 
+import example.project.entity.Identity;
 import example.project.entity.User;
 import example.project.usecase.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,11 @@ public class JPAUserRepository implements UserRepository {
     @Override
     public Mono<User> findByUserName(String userName) {
         return this.repository.findByUserName(userName).map(JPAUserEntity::toDomain);
+    }
+
+    @Override
+    public Mono<User> findByUserId(Identity userId) {
+        return this.repository.findById(userId.getId()).map(JPAUserEntity::toDomain);
     }
 
     @Override
